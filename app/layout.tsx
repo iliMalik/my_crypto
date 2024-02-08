8b2419f9578d6d2ebe929e0886eff6c698b544cd
Ilyas_Malik <candorilyas@gmail.com>
2024-02-08 06:31:37 -0500

    spped insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import '@/services/styling/globals.css';
 import NavBar from '@/components/navbar';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
   title: 'IlyasMalik',
@@ -16,7 +17,10 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <NavBar />
-        <main className="px-8 py-20  mx-auto">{children}</main>
+        <main className="px-8 py-20  mx-auto">
+          {children}
+          <SpeedInsights />
+        </main>
       </body>
     </html>
   );
